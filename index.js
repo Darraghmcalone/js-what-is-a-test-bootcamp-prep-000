30259d8bbe6e351ba45bbaf95d41a51e345ff953
darraghmcalone@hotmail.co.uk <darraghmcalone@hotmail.co.uk>
2017-10-19 18:51:44 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,8 +4,6 @@ var height = 74
 var message = `${name} is ${height} inches tall`
 
 module.exports = {name, height, message}
-<<<<<<< HEAD
+
 console.log(message)
-=======
-console.log(message)
->>>>>>> b414d96b5ad234d4a5ab5cb8cac3b83d452ee75a
+
